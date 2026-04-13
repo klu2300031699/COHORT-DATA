511e3b14c0e15d8a34e688415afd17c065a43582
klu2300031699 <2300031699cseh@gmail.com>
2026-04-13 09:35:14 +0530

    added y23 final attanedance

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -167,13 +167,13 @@ export default function Header({ user, onLogout }) {
                   Report
                 </button>
               )}
-              {['Gnanesh', '4868', '1277', '8068', '8215'].includes(user.id) && (
+              {['Gnanesh', '4868', '1277', '8068', '8215', '8964', '6799', '5291', '9122'].includes(user.id) && (
                 <button
                   className="registration-header__report-btn"
                   onClick={() => setShowCohortTracker(true)}
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
                   </svg>
                   Cohort Tracker
                 </button>
